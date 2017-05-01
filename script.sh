380ce774b2ca8c07659305b1f2c13b995ea4fbb1
Nathan McGinn <nmcginn@users.noreply.github.com>
2017-05-01 18:58:31 -0500

    init

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# dependencies, 1st time only
+#sudo apt install virtualenv
+
+# R dependencies for baseline
+wget https://cran.r-project.org/src/contrib/Archive/survival/survival_2.39-4.tar.gz
+sudo R CMD INSTALL survival_2.39-4.tar.gz
+
+# python virtualenv
+virtualenv ~/env && source ~/env/bin/activate
+pip install rpy2
+
+# view native dependencies
+ldd /usr/lib/R/bin/exec/R
+
+# start assembling binaries
+mkdir lambda
+mkdir lambda/lib
+cp -r /usr/lib/R/* lambda/
+cp /usr/lib/R/lib/libR.so lambda/lib/libR.so
+
+# pontentially missing other libraries as well, make it work
+cp /usr/lib/x86_64-linux-gnu/libgomp.so.1 lambda/lib/
+cp /usr/lib/libblas.so.3 lambda/lib/
+cp /usr/lib/liblapack.so.3 lambda/lib/
+
+# and finally the big enchilada
+cp lambda/bin/exec/R lambda/
+
+# and some python junk too
+cp -r $VIRTUAL_ENV/lib/python2.7/site-packages/* lambda/
+cp -r $VIRTUAL_ENV/lib/python2.7/site-packages/singledispatch* lambda/
+
+# MAKE THE PACKAGE
+cd lambda/
+zip -r9 here_we_go_boys.zip *
+
+# after this, upload to the S3s, make sure things are proper, and deploy to lambda
+
