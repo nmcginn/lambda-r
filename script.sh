34d4f03612ecf918c3b18c0537efffb77e5f85e9
Nathan McGinn <nathan.mcginn@climate.com>
2017-05-02 13:19:26 -0500

    using amazonlinux

--- script.sh
+++ script.sh
@@ -2,33 +2,32 @@
 
 # python virtualenv
 virtualenv ~/env && source ~/env/bin/activate
-pip install rpy2 awscli
 
 # view native dependencies
-ldd /usr/lib/R/bin/exec/R
+ldd /usr/lib64/R/bin/exec/R
 
 # start assembling binaries
-cp -r /usr/lib/R/* lambda/
-cp /usr/lib/R/lib/libR.so lambda/lib/libR.so
+cp -r /usr/lib64/R/* lambda/
+cp /usr/lib64/R/lib/libR.so lambda/lib/libR.so
 
 # pontentially missing other libraries as well, make it work
-cp /usr/lib/x86_64-linux-gnu/libgomp.so.1 lambda/lib/
-cp /usr/lib/x86_64-linux-gnu/libgfortran.so.3 lambda/lib/
-cp /usr/lib/x86_64-linux-gnu/libquadmath.so.0 lambda/lib/
-cp /lib/x86_64-linux-gnu/libm.so.6 lambda/lib/
-#cp /lib/x86_64-linux-gnu/libc.so.6 lambda/lib/
-cp /lib/x86_64-linux-gnu/libpcre.so.3 lambda/lib/
-cp /lib/x86_64-linux-gnu/libbz2.so.1.0 lambda/lib/
-#cp /lib/x86_64-linux-gnu/libgcc_s.so.1 lambda/lib/
-cp /usr/lib/libblas.so.3 lambda/lib/
-cp /usr/lib/liblapack.so.3 lambda/lib/
+cp /usr/lib64/libgomp.so.1 lambda/lib/
+cp /usr/lib64/libgfortran.so.3 lambda/lib/
+cp /usr/lib64/libquadmath.so.0 lambda/lib/
+#cp /lib/libm.so.6 lambda/lib/
+#cp /lib/libc.so.6 lambda/lib/
+#cp /lib/libpcre.so.3 lambda/lib/
+#cp /lib/libbz2.so.1.0 lambda/lib/
+#cp /lib/libgcc_s.so.1 lambda/lib/
+#cp /usr/lib64/libblas.so.3 lambda/lib/
+#cp /usr/lib64/liblapack.so.3 lambda/lib/
 
 # and finally the big enchilada
 cp lambda/bin/exec/R lambda/
 
 # and some python junk too
 cp -r $VIRTUAL_ENV/lib/python2.7/site-packages/* lambda/
-cp -r $VIRTUAL_ENV/lib/python2.7/site-packages/singledispatch* lambda/
+#cp -r $VIRTUAL_ENV/lib/python2.7/site-packages/singledispatch* lambda/
 
 # MAKE THE PACKAGE
 cd lambda/
